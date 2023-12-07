b6f7c90cc45c74030538ced3f79ccfee13d537d8
Yeeloman <yami.onlyme@gmail.com>
2023-12-07 03:23:31 +0100

    add fixed task
    im assuming it was sh -c whats wrong and change it to tee for some reason

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# script to set up clean server
+# a Bash script that sets up your web servers
 sudo apt-get update
-sudo apt-get install -y nginx
-sudo mkdir /data/
-sudo mkdir /data/web_static/
-sudo mkdir /data/web_static/releases/
-sudo mkdir /data/web_static/shared/
-sudo mkdir /data/web_static/releases/test/
+sudo apt-get install nginx -y
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
 echo "<!DOCTYPE html>
 <html>
     <head>
@@ -16,7 +15,8 @@ echo "<!DOCTYPE html>
         <h1>Welcome to AirBnB clone - Deploy static!</h1>
     </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.bak
+sudo sed -i '39 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
