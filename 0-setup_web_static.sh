af6154295b9e827d6564f4f5a220b18fd91922e1
Yeeloman <yami.onlyme@gmail.com>
2023-12-07 03:10:21 +0100

    fix test

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,16 +8,16 @@ sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 sudo sh -c 'echo "<!DOCTYPE html>
 <html>
-<head>
-    <title>Your Page Title</title>
-</head>
-<body>
-    <h1>Welcome to Your Website</h1>
-</body>
+  <head>
+      <title>Your Page Title</title>
+  </head>
+  <body>
+      <h1>Welcome to Your Website</h1>
+  </body>
 </html>
 " > /data/web_static/releases/test/index.html'
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.bak
-sudo sed -i '2 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i '39 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
