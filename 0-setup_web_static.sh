8eb36524e807a5b0247ae1315bb6945c5911b3cd
Yeeloman <yami.onlyme@gmail.com>
2023-12-07 03:28:37 +0100

    definitly fix task 0
    im hoping at least

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 # a Bash script that sets up your web servers
 sudo apt-get update
-sudo apt-get install nginx -y
-
-sudo mkdir -p /data/web_static/releases/test/
+sudo apt-get install -y nginx
 sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
 echo "<!DOCTYPE html>
 <html>
     <head>
@@ -15,8 +13,7 @@ echo "<!DOCTYPE html>
         <h1>Welcome to AirBnB clone - Deploy static!</h1>
     </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.bak
-sudo sed -i '39 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
