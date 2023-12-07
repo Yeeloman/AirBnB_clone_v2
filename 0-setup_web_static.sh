954f86fef9e68d2229587df5cbb77647d8d6bcc4
Yeeloman <yami.onlyme@gmail.com>
2023-12-07 03:19:35 +0100

    tryna fix task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env bash
-# a Bash script that sets up your web servers
+# script to set up clean server
 sudo apt-get update
-sudo apt-get install nginx -y
-
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-sudo touch /data/web_static/releases/test/index.html
-sudo sh -c 'echo "<!DOCTYPE html>
+sudo apt-get install -y nginx
+sudo mkdir /data/
+sudo mkdir /data/web_static/
+sudo mkdir /data/web_static/releases/
+sudo mkdir /data/web_static/shared/
+sudo mkdir /data/web_static/releases/test/
+echo "<!DOCTYPE html>
 <html>
-  <head>
-      <title>Your Page Title</title>
-  </head>
-  <body>
-      <h1>Welcome to Your Website</h1>
-  </body>
-</html>
-" > /data/web_static/releases/test/index.html'
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+    <head>
+        <title>Home - AirBnB clone</title>
+    </head>
+    <body>
+        <h1>Welcome to AirBnB clone - Deploy static!</h1>
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.bak
-sudo sed -i '39 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
