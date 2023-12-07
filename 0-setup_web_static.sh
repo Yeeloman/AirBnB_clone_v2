ca6f3d989d436af14f0676d68ec7b6aefa314eb4
Yeeloman <yami.onlyme@gmail.com>
2023-12-07 02:36:55 +0100

    add file 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# a Bash script that sets up your web servers
+# for the deployment of web_static.
+sudo apt-get update
+sudo apt-get install nginx -y
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+sudo touch /data/web_static/releases/test/index.html
+sudo sh -c 'echo "<!DOCTYPE html>
+<html>
+<head>
+    <title>Your Page Title</title>
+</head>
+<body>
+    <h1>Welcome to Your Website</h1>
+</body>
+</html>
+" > /data/web_static/releases/test/index.html'
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo cp /etc/nginx/sites-enabled/default /etc/nginx/sites-enabled/default.bak
+sudo sed -i '2 i\ \tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
