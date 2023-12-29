1abdb16cc86452e1d57dc2ba30b50abc7e684364
Yeeloman <yami.onlyme@gmail.com>
2023-12-29 22:18:31 +0100

    add first 7 tasks

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""a script that starts a flask application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """display hello hbnb"""
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb_page():
+    """display hbnb"""
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
