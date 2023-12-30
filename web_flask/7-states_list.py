ac25303f9efedc06a90fa1d939be198314c24796
Yeeloman <yami.onlyme@gmail.com>
2023-12-30 14:37:54 +0100

    still

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,12 +8,6 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_database(arg):
-    """closes the current database"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list_page():
     """returns a list of all states"""
@@ -21,5 +15,11 @@ def states_list_page():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def close_database(arg):
+    """closes the current database"""
+    storage.close()
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
