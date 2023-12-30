642fa7d5d18c8dc0c1048e8b09375c7973fe1540
Yeeloman <yami.onlyme@gmail.com>
2023-12-30 15:03:21 +0100

    wanna kill myself with alx

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" flask web app"""
+"""a script that starts a flask application"""
+
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -8,19 +9,17 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def HBNB_states_list():
-    """
-    display a HTML page with the list of all State objects in DBStorage
-    """
+def states_list_page():
+    """returns a list of all states"""
     states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown_app(self, exception=None):
-    """ remove the current SQLAlchemy Session """
+def close_database(arg):
+    """closes the current database"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
