a62cb1dc21ed61261a526f640a9b8b2900a3ffc9
Yeeloman <yami.onlyme@gmail.com>
2023-12-30 14:33:16 +0100

    add

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -17,7 +17,7 @@ def close_database(arg):
 @app.route('/states_list', strict_slashes=False)
 def states_list_page():
     """returns a list of all states"""
-    states = storage.all(State).values()
+    states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
